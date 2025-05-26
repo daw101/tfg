842e6844e7bc7ab47d12721f62fbaff0038c54d5
daw101 <jdiazdeceriom04@gmail.com>
2025-05-26 20:59:36 +0200

    Update Cart.php

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -11,19 +11,16 @@ class Cart extends Model
 
     protected $fillable = ['user_id'];
 
-    // Un carrito pertenece a un usuario
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // Un carrito puede tener muchos productos (muchos a muchos)
     public function products()
     {
         return $this->belongsToMany(Product::class)->withPivot('quantity')->withTimestamps();
     }
 
-    // Un carrito puede tener varios métodos de pago
     public function paymentMethods()
     {
         return $this->hasMany(PaymentMethod::class);
