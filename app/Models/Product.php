a651f0c006ce488d4739b421d5b3b66a57d1474c
daw101 <jdiazdeceriom04@gmail.com>
2025-05-26 21:00:27 +0200

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,13 +11,11 @@ class Product extends Model
 
     protected $fillable = ['name', 'description', 'price', 'stock', 'image', 'category_id'];
 
-    // Un producto pertenece a una categoría
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
-    // Un producto puede estar en muchos carritos (muchos a muchos)
     public function carts()
     {
         return $this->belongsToMany(Cart::class)->withPivot('quantity')->withTimestamps();
