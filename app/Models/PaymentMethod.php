506a406b09883c6412377736ce1057dc34d2acd5
daw101 <jdiazdeceriom04@gmail.com>
2025-05-26 21:00:18 +0200

    Update PaymentMethod.php

--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -11,19 +11,16 @@ class PaymentMethod extends Model
 
     protected $fillable = ['name', 'user_id', 'cart_id'];
 
-    // Un método de pago pertenece a un usuario
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // Un método de pago pertenece a un carrito
     public function cart()
     {
         return $this->belongsTo(Cart::class);
     }
 
-    // Un método de pago puede tener muchos pagos
     public function payments()
     {
         return $this->hasMany(Payment::class);
