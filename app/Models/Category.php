00728bbd7c1148a9686835bd95a860bb9cf941ae
daw101 <jdiazdeceriom04@gmail.com>
2025-05-26 20:59:46 +0200

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,7 +11,6 @@ class Category extends Model
 
     protected $fillable = ['name'];
 
-    // Una categoría tiene muchos productos
     public function products()
     {
         return $this->hasMany(Product::class);
