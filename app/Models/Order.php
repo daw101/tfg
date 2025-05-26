b3a5258c6ba635845d19d270e1a1aadc2ec64fc4
daw101 <jdiazdeceriom04@gmail.com>
2025-05-26 20:59:58 +0200

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,19 +11,16 @@ class Order extends Model
 
     protected $fillable = ['user_id', 'cart_id', 'order_date', 'total'];
 
-    // Un pedido pertenece a un usuario
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // Un pedido pertenece a un carrito
     public function cart()
     {
         return $this->belongsTo(Cart::class);
     }
 
-    // Un pedido tiene muchos pagos
     public function payments()
     {
         return $this->hasMany(Payment::class);
